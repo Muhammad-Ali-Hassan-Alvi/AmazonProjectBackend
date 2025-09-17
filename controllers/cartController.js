7f5d2e995e68390089b3e40b86bc55ebacefaf5e
Muhammad-Ali-Hassan-Alvi <muhammadalialvi646@gmail.com>
2025-09-17 23:44:21 +0500

    Completed the controllers for the Cart

---
+++ controllers/cartController.js
@@ -0,0 +1,198 @@
+import Cart from "../models/Cart.js";
+import Buyer from "../models/Buyer.js";
+import { Product } from "../models/Product.js";
+
+export const getUserCart = async (req, res) => {
+  try {
+    const user = await Buyer.findOne({ userId: req.user._id });
+    if (!user) {
+      return res.status(404).json({ message: "User not Found" });
+    }
+
+    const cart = await Cart.findOne({
+      userId: user._id,
+      status: "active",
+    }).populate({ path: "items.productId", select: "name price images stock" });
+    if (!cart || cart.items.length === 0) {
+      return res.status(200).json({
+        message: "Cart not Found",
+        data: {
+          _id: cart?._id,
+          items: [],
+          default: 0,
+        },
+      });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Cart Fetched successfully.", data: cart });
+  } catch (error) {
+    console.error("Error fetching user cart:", error);
+    return res
+      .status(500)
+      .json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const addToCart = async (req, res) => {
+  try {
+    const { productId, quantity } = req.body;
+
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ message: "Product not Found" });
+    }
+
+    if (product.stock < quantity) {
+      return res.status(400).json({ message: "Not enough Stock" });
+    }
+
+    const buyer = await Buyer.findOne({ userId: req.user.id });
+    if (!buyer) {
+      return res.status(404).json({ message: "Buyers record not Found" });
+    }
+
+    let cart = await Cart.findOne({ userId: req.user.id });
+
+    if (!cart) {
+      cart = new Cart({
+        userId: buyer._id,
+        items: [],
+      });
+    }
+
+    const itemIndex = cart.items.findIndex(
+      (p) => p.productId.toString() === productId
+    );
+
+    if (itemIndex > -1) {
+      cart.items[itemIndex].quantity += quantity;
+    } else {
+      cart.items.push({
+        productId,
+        quantity,
+        price: product.price,
+      });
+    }
+
+    await cart.save();
+
+    const populatedCart = await cart.populate("items.productId");
+
+    return res.status(200).json({
+      message: "Item successfully added to cart",
+      data: populatedCart,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const updateItemQuantity = async (req, res) => {
+  const { productId } = req.params;
+  const { quantity } = req.body;
+
+  try {
+    const buyer = await Buyer.findOne({ userId: req.user.id });
+    if (!buyer) {
+      return res.status(404).json({ message: "Buyer profile not found" });
+    }
+
+    const cart = await Cart.findOne({ userId: buyer._id });
+
+    if (!cart) {
+      return res.status(404).json({ message: "Nothing in the cart " });
+    }
+
+    const itemIndex = cart.items.findIndex(
+      (p) => p.productId.toString() === productId
+    );
+    if (itemIndex === -1) {
+      return res.status(404).json({ message: "Item not found in cart" });
+    }
+
+    const product = await Product.findById(productId);
+    if (product < quantity) {
+      return res.status(404).json({ message: "Not enough stock" });
+    }
+
+    cart.items[itemIndex].quantity === quantity;
+
+    await cart.save();
+
+    const populatedCart = await cart.populate("items.productId");
+
+    return res
+      .status(200)
+      .json({ message: "Cart is updated", data: populatedCart });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+export const removeItem = async (req, res) => {
+  try {
+    const productId = req.params;
+    const buyer = await Buyer.findOne({ userId: req.user.id });
+
+    if (!buyer) {
+      return res.status(404).json({ message: "Buyer Data not Found" });
+    }
+
+    const cart = await Cart.findOne({ userId: buyer._id });
+
+    if (!cart) {
+      return res.status(404).json({ message: "Cart is empty" });
+    }
+
+    const updatedCart = await Cart.findByIdAndUpdate(
+      cart._id,
+      {
+        $pull: { items: { productId: productId } },
+      },
+      { new: true }
+    ).populate("items.productId");
+
+    if (!updatedCart) {
+      return res.status(400).json({ message: "Unable to delete item" });
+    }
+
+    await updatedCart.save();
+    return res
+      .status(404)
+      .json({ message: "Cart Updated Successfully ", data: updatedCart });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", data: error.message });
+  }
+};
+
+export default clearCart = async (req, res) => {
+  try {
+    const buyer = await Buyer.findOne({ userId: req.user.id });
+    if (!buyer) {
+      return res.status(404).json({ message: "Buyer not found" });
+    }
+
+    const cart = await Cart.findOne({ userId: buyer._id });
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not Found for this User" });
+    }
+
+    cart.items = [];
+
+    await cart.save();
+
+    return res
+      .status(200)
+      .json({ message: "Cart Updated Successfully", data: cart });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server Error", data: error.message });
+  }
+};
---
+++ models/Cart.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const cartItemSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+      default: 1,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Buyer",
+      required: true,
+    },
+    items: [cartItemSchema],
+    status: {
+      type: String,
+      enum: ["active", "abandoned", "ordered"],
+      default: active,
+    },
+    total: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+cartSchema.pre("save", function (next) {
+  this.total = this.items.reduce(
+    (sum, item) => sum + item.quantity * item.price,
+    0
+  );
+  next();
+});
+
+export default mongoose.model("Cart", cartSchema);
